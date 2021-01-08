f29a4378358c1f0f46e2f7c77e316e03b92bb95b
ndxken <ndxken@gmail.com>
2021-01-08 15:16:39 +0700

    full

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(".courses__slick--top").slick({
   infinite: false,
   speed: 300,
   slidesToShow: 3,
-  slidesToScroll: 3,
+  slidesToScroll: 2,
   prevArrow:
     '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
   nextArrow:
@@ -13,7 +13,7 @@ $(".courses__slick--top").slick({
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 2,
       },
     },
     {
